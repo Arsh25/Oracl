51ea939a2aa8347112c1d579f9b4d9ee4b1e0245
mperry37 <mperry37@alaska.edu>
2019-10-24 12:15:08 -0800

    fix: returns error

---
+++ pymongoinsert.py
@@ -0,0 +1,15 @@
+import pymongo
+import logging
+from pymongo import MongoClient
+from pymongo import errors as mongoerrors
+
+def insert_data(client, database, coloumn, data)-> bool:
+    dbclient = MongoClient(client)
+    db = dbclient[database]
+    dbcol = db[coloumn]
+    try:
+        dbcol.insert_many(data)
+    except mongoerrors.PyMongoError as e:
+        logging.exception(e)
+        return False, e
+    return True
