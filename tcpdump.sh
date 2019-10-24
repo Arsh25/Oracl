86291aa0904acad95663b8ccde6160f2f41953fa
mperry37 <mperry37@alaska.edu>
2019-10-24 10:33:56 -0800

    init: creates shell script for running tcmpdump

---
+++ tcpdump.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Welcome to pcapture set up for Oracl"
+echo "recommened that you check you network device name with ifconfig"
+echo "Please input your inputdevice that will be used to capture traffic"
+read inputdevice
+
+echo "is ${inputdevice} correct,"
+read -p "Continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) ;;
+  n|N ) echo "goodbye" &&  exit;;
+  * ) echo "invalid" && exit;;
+esac
+echo
+echo "files will be stored in /var/tmp/capture_date_time_captured.pcap creating a new file every 10 mins or 10mb in file size."
+
+tcpdump -i ${inputdevice} -w /var/tmp/capture-%m-%d_%H.%M.%S.pcap -G 600 -C 10
